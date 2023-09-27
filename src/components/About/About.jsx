7475f2a23cf46a2b66fd628849998c4c7733a07c
Badejo Emmanuel <badejoemmanuel805@gmail.com>
2023-09-27 05:03:10 -0500

    First commit

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const About = () => {
   return (
       <section id="about" className='flex'>
-        <div className='w-1/2 bg-slate-800 p-5 rounded-lg flex flex-col space-y-5 backdrop:blur-3xl'>
+        <div className='lg:w-1/2 bg-slate-800 p-5 rounded-lg flex flex-col space-y-5 backdrop:blur-3xl'>
             <h1 className='text-5xl font-bold text-slate-300'>About Me</h1>
             <p className='text-slate-400'>I am Emmanuel a passionate front-end developer with a creative flair and a love for creatin excepyional digital experiences. With a strong foundation in HTML,CSS,JAVASCRIPT,REACT .My Journey into the world of web development began about few years ago during my secondary school days since i was a teenager. I was instantly captivated by the endless possibilities of the web and its power to connect people and ideas across the globe.</p>
          
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section className='flex  my-[12rem] items-center justify-between'>
-        <div className='w-1/2'>
+    <section className='flex flex-col md:flex-row  my-[12rem] items-center justify-between'>
+        <div className='md:w-1/2'>
             <h1 className='text-3xl text-slate-400'>Frontend developer.</h1>
             <p className='text-5xl mt-5 font-semibold '>Hello!, I'm <span className='text-red-600'>Emmanuel</span> Badejo </p>
         </div>
-        <div className='w-[60%] border rouned-lg border-slate-700'><img  src="/Poster.jpg" className='w-full rounded-lg h-[30rem]' alt="profileimage" /></div>
+        <div className='md:w-[60%] border rouned-lg border-slate-700'><img  src="/Poster.jpg" className='w-full rounded-lg h-[30rem]' alt="profileimage" /></div>
     </section>
   )
 }
